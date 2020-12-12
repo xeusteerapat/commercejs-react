1883fd4c844dddf974c8b2c2d985fbdcb17c694f
xeusteerapat <xeus085@gmail.com>
2020-12-12 22:20:39 +0700

    [add] cart item

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Grid, Container, Typography } from '@material-ui/core';
 import useStyles from './styles';
+import CartItem from './cart-items/CartItem';
 
 const Cart = ({ cart }) => {
   const classes = useStyles();
@@ -17,7 +18,7 @@ const Cart = ({ cart }) => {
         <Grid container spacing={3}>
           {cart.line_items.map(item => (
             <Grid item xs={12} sm={4} key={item.id}>
-              <div>{item.name}</div>
+              <CartItem item={item} />
             </Grid>
           ))}
         </Grid>
