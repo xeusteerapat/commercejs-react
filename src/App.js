7c72c4f8a072e1b34ac52c78570d3253003c672a
xeusteerapat <xeus085@gmail.com>
2020-12-12 21:20:41 +0700

    [add] commerce sdk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './libs/commerce';
 import Navbar from './components/navbar/Navbar';
 import Products from './components/products/Products';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
