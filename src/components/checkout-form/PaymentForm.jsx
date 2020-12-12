bd5de93a63b5edabd679084098b4affc55b6f1c5
xeusteerapat <xeus085@gmail.com>
2020-12-13 02:04:58 +0700

    [edit] stripe  payment form

--- src/components/checkout-form/PaymentForm.jsx
+++ src/components/checkout-form/PaymentForm.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
 
-const PaymentForm = () => {
-  return <div>payment</div>;
+const stripePromise = loadStripe('..');
+
+const PaymentForm = ({ checkoutToken, backstep }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography
+        variant='h6'
+        gutterBottom
+        style={{
+          margin: '20px 0',
+        }}
+      >
+        Payment Method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>
+          {({ elements, stripe }) => (
+            <form>
+              <CardElement />
+              <br />
+              <br />
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Button variant='outlined' onClick={backstep}>
+                  Back
+                </Button>
+                <Button
+                  type='submit'
+                  variant='contained'
+                  disabled={!stripe}
+                  color='primary'
+                >
+                  Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                </Button>
+              </div>
+            </form>
+          )}
+        </ElementsConsumer>
+      </Elements>
+    </>
+  );
 };
 
 export default PaymentForm;
