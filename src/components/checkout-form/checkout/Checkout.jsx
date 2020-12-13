e3e30a6c67943fb6e2065f4cd162e2cfa9b8f294
xeusteerapat <xeus085@gmail.com>
2020-12-13 15:02:07 +0700

    [edit] add css baseline for mobile responsive

--- src/components/checkout-form/checkout/Checkout.jsx
+++ src/components/checkout-form/checkout/Checkout.jsx
@@ -8,44 +8,55 @@ import {
   CircularProgress,
   Divider,
   Button,
+  CssBaseline,
 } from '@material-ui/core';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
 import { commerce } from '../../../libs/commerce';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = ({ cart, order, onCaptureCheckout, errorMsg }) => {
-  const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState({});
-
-  useEffect(() => {
-    const generateToken = async () => {
-      try {
-        const token = await commerce.checkout.generateToken(cart.id, {
-          type: 'cart',
-        });
-
-        setCheckoutToken(token);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    generateToken();
-  }, [cart]);
+  const [isFinished, setIsFinished] = useState(false);
+  const classes = useStyles();
+  const history = useHistory();
 
   const nextStep = () => setActiveStep(prevActiveStep => prevActiveStep + 1);
   const backStep = () => setActiveStep(prevActiveStep => prevActiveStep - 1);
 
-  const next = data => {
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, {
+            type: 'cart',
+          });
+
+          setCheckoutToken(token);
+        } catch (err) {
+          if (activeStep !== steps.length) history.push('/');
+        }
+      };
+
+      generateToken();
+    }
+  }, [cart]);
+
+  const test = data => {
     setShippingData(data);
 
     nextStep();
+  };
+
+  const timeout = () => {
+    setTimeout(() => {
+      setIsFinished(true);
+    }, 3000);
   };
 
   let Confirmation = () =>
@@ -59,6 +70,19 @@ const Checkout = ({ cart, order, onCaptureCheckout, errorMsg }) => {
             <Typography variant='subtitle2'>
               Order ref: {order.customer_reference}
             </Typography>
+          </Typography>
+        </div>
+        <br />
+        <Button component={Link} to='/' variant='outlined'>
+          Back to Home
+        </Button>
+      </>
+    ) : isFinished ? (
+      <>
+        <div>
+          <Typography variant='h5'>
+            Thank you for your purchase
+            <Divider className={classes.divider} />
           </Typography>
         </div>
         <br />
@@ -83,7 +107,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, errorMsg }) => {
 
   const Form = () =>
     activeStep === 0 ? (
-      <AddressForm checkoutToken={checkoutToken} next={next} />
+      <AddressForm checkoutToken={checkoutToken} test={test} />
     ) : (
       <PaymentForm
         checkoutToken={checkoutToken}
@@ -91,11 +115,13 @@ const Checkout = ({ cart, order, onCaptureCheckout, errorMsg }) => {
         backStep={backStep}
         shippingData={shippingData}
         onCaptureCheckout={onCaptureCheckout}
+        timeout={timeout}
       />
     );
 
   return (
     <>
+      <CssBaseline />
       <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
