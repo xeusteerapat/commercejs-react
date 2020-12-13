35d733427ebb47ef2604a14f57a0b6c53bca7969
xeusteerapat <xeus085@gmail.com>
2020-12-13 14:14:04 +0700

    [edit] update default value for form input

--- src/components/checkout-form/FormInput.jsx
+++ src/components/checkout-form/FormInput.jsx
@@ -7,6 +7,7 @@ const FormInput = ({ name, label, required }) => {
   return (
     <Grid item xs={12} sm={6}>
       <Controller
+        defaultValue=''
         as={TextField}
         control={control}
         fullWidth
